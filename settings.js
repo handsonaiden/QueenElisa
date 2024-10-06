07d50ad355b4260feacbb1303fe610721805837a
handsonaiden <164012070+handsonaiden@users.noreply.github.com>
2024-10-06 09:02:41 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 module.exports = {
-  SESSION_ID: '',
+  SESSION_ID: 'QUEENELISA;;;==wb1MXS4okMVVUZuhXWhhmWixkVHVkapRTOXZFZxJzQHFGUIdnNP9kRsJjejQVYSV1aO10dpwd=984239637452',
   ALIVE_REACT: '👋',
   ALIVE_MESSAGE: `*Hello @name [ And @gname @members Number of Members ] I am Alive Now !*
 
